ee8b7e6711f34e3733d6a9eb0bd9a94118058ae8
Jochen Schalanda <j.schalanda@smarchive.de>
2012-09-06 15:43:17 +0200

    Add very basic Pig support

--- manifests/pig.pp
+++ manifests/pig.pp
@@ -1,2 +1,9 @@
-class cdh::pig {
+class cdh::pig(
+  $package = $cdh::pig::params::package,
+  $version = $cdh::pig::params::version,
+) inherits cdh::pig::params {
+
+  package { $package:
+    ensure => $version,
+  }
 }
--- manifests/pig/params.pp
+++ manifests/pig/params.pp
@@ -1,11 +1,11 @@
 class cdh::pig::params {
-    $pig_package = $::cdh_pig_package ? {
-        undef   => 'pig',
-        default => $::cdh_pig_package,
-    }
+  $package = $::cdh_pig_package ? {
+    undef   => 'pig',
+    default => $::cdh_pig_package,
+  }
 
-    $pig_version = $::cdh_pig_version ? {
-        undef   => 'installed',
-        default => $::cdh_pig_version,
-    }
+  $version = $::cdh_pig_version ? {
+    undef   => 'installed',
+    default => $::cdh_pig_version,
+  }
 }
