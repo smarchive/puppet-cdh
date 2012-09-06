38634f91b2ea001523d8da94e6c245cd65627f4c
Jochen Schalanda <j.schalanda@smarchive.de>
2012-09-06 15:37:28 +0200

    Add basic Sqoop support

--- manifests/sqoop.pp
+++ manifests/sqoop.pp
@@ -1,2 +1,9 @@
-class cdh::sqoop {
+class cdh::sqoop(
+  $package             = $cdh::sqoop::params::package,
+  $version             = $cdh::sqoop::params::version,
+) inherits cdh::sqoop::params {
+
+  package { $package:
+    ensure => $version,
+  }
 }
--- manifests/sqoop/params.pp
+++ manifests/sqoop/params.pp
@@ -1,10 +1,10 @@
 class cdh::sqoop::params {
-    $sqoop_package = $::cdh_sqoop_package ? {
+    $package = $::cdh_sqoop_package ? {
         undef   => 'sqoop',
         default => $::cdh_sqoop_package,
     }
 
-    $sqoop_version = $::cdh_sqoop_version ? {
+    $version = $::cdh_sqoop_version ? {
         undef   => 'installed',
         default => $::cdh_sqoop_version,
     }
